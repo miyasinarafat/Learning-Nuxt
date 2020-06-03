230d41b0081285112da54ddfdc0de55c98e2fee9
iarafat <miyasinarafat@gmail.com>
2020-06-03 12:52:39 +0600

    added navbar into the layouts

---
+++ components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/post">Blog</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <navbar></navbar>
     <nuxt />
   </div>
 </template>
+
+<script>
+  import Navbar from "../components/Navbar";
+
+  export default {
+    components: {
+      Navbar
+    }
+  }
+</script>
