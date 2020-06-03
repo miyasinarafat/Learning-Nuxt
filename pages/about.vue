f0f6675a9ee71f9062d751d89ddb373d142eba4b
iarafat <miyasinarafat@gmail.com>
2020-06-03 14:23:08 +0600

    added pages

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">About page</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "about"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">Contact page</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "contact"
+    }
+</script>
+
+<style scoped>
+
+</style>
